055c97e74d0b23246a1de9faf7f4ded31021a1e0
JR Marquez <Jr14marquez@gmail.com>
2017-04-22 17:34:52 -0400

    fix error in request

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server = http.createServer(function(request, response){
 	var filePath = false;
 
 	if(request.url == '/'){ filePath = 'public/index.html'; }
-	else { filePath = 'public' + requst.url; }
+	else { filePath = 'public' + request.url; }
 
 	var absPath = './' + filePath;
 	serveStatic(response,cache,absPath);
